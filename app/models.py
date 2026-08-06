4691770ff835a0c07d8c53f040596b024d8999c6
EC2 Default User <ec2-user@ip-172-31-31-22.eu-west-1.compute.internal>
2026-08-06 21:48:55 +0000

    modify database

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from sqlalchemy import Column, Integer, Boolean, String, Date
 
 class Todo(Base):
     __tablename__ = "todo_list"
-    __table_args__ = {"schema": "todo_db"}
         
     id = Column(Integer, primary_key=True, autoincrement=True)
     task = Column(String)
