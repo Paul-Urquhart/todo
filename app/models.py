42b3e9d357892dcc471613cbde482c442954e2b0
Paul-Urquhart <paul418.urquhart108@gmail.com>
2026-08-14 22:34:24 +0000

    create user model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .db import Base
-from sqlalchemy import Column, Integer, Boolean, String, Date
+from sqlalchemy import Column, Integer, Boolean, String, Date, ForeignKey
 
 class Todo(Base):
     __tablename__ = "todo_list"
@@ -8,3 +8,11 @@ class Todo(Base):
     task = Column(String)
     done = Column(Boolean)
     due_date = Column(Date)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True) # NULLABLE temporarily
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
